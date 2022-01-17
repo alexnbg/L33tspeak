1a54d74992ff694e85690dbe31191ba1153f7b55
Alex <alexnb.bg@gmail.com>
2022-01-17 14:23:27 +0200

    Added .gitignore file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 dict_L33t = {
     'a': '4',
     'b': '6',
@@ -30,36 +29,37 @@ dict_L33t = {
 }
 
 
-
-
 # new methods
 
-def eng_L33t(string:str):
+def eng_L33t(string: str):
     ans = string
     for ch in string:
         ans = ans.replace(ch, dict_L33t[ch])
-    print ('Translated phrase:')
-    print (ans)
+    print('Translated phrase:')
+    print(ans)
 
-def L33t_eng(string:str):
+
+def L33t_eng(string: str):
     ans = string
     for el in dict_L33t.values():
-        ans = ans.replace(el, list(dict_L33t.keys())[list(dict_L33t.values()).index(el)])
-    print ('Translated phrase:')
-    print (ans)
+        ans = ans.replace(el, list(dict_L33t.keys())[
+                          list(dict_L33t.values()).index(el)])
+    print('Translated phrase:')
+    print(ans)
 
 
-def english_to_L33t(string:str):
+def english_to_L33t(string: str):
     ans = []
     for ch in string:
         if ch in dict_L33t.keys():
             ans.append(dict_L33t[ch])
         else:
             print('Unknown character: ', ch)
-    print ('Translated phrase:')
-    print (''.join(ans))
+    print('Translated phrase:')
+    print(''.join(ans))
 
-def L33t_to_english(string:str):
+
+def L33t_to_english(string: str):
     ans = []
     revdict = {v: k for k, v in dict_L33t.items()}
     i = 0
@@ -76,13 +76,14 @@ def L33t_to_english(string:str):
         else:
             print('Unknown character')
             i += 1
-    print ('Translated phrase:')
-    print (''.join(ans))
+    print('Translated phrase:')
+    print(''.join(ans))
+
 
 def translating():
     phrase = str(input('Enter phrase: ')).lower()
 
-    diff = ['1','3','4','5','6','7','/','(',')','`']
+    diff = ['1', '3', '4', '5', '6', '7', '/', '(', ')', '`']
 
     leet = None
 
@@ -103,3 +104,5 @@ def translating():
 
 
 translating()
+
+print(123)
